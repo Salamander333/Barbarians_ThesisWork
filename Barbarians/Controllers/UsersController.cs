8bae93c615c03f6c79816c2bae59dad3422fa6c9
Salamander333 <y.glushkov@abv.bg>
2020-03-02 17:03:43 +0200

    added custom register and login pages

---
+++ Barbarians/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using Barbarians.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Barbarians.Controllers
+{
+    [AllowAnonymous]
+    public class UsersController : Controller
+    {
+        private readonly UserManager<ApplicationUser> user;
+
+        public UsersController(UserManager<ApplicationUser> user)
+        {
+            this.user = user;
+        }
+
+        public IActionResult Login()
+        {
+            ViewData["Title"] = "Login";
+            return this.View();
+        }
+
+        public IActionResult Register()
+        {
+            ViewData["Title"] = "Register";
+            return this.View();
+        }
+    }
+}
