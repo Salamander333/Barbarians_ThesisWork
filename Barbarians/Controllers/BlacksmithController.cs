967a7814294ed5803bc98c6cd3aa56931fa673f4
Salamander333 <y.glushkov@abv.bg>
2020-04-03 19:17:32 +0300

    added blacksmith partial views and navigation

---
+++ Barbarians/Controllers/BlacksmithController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Barbarians.Controllers
+{
+    public class BlacksmithController : Controller
+    {
+        [HttpGet]
+        [Authorize]
+        public IActionResult Index(string craft)
+        {
+            var partialName = "";
+
+            if (craft == "Chest")
+            {
+                partialName = "_BlacksmithChest";
+            }
+            else if (craft == "Leggings")
+            {
+                partialName = "_BlacksmithLeggings";
+            }
+            else if (craft == "Boots")
+            {
+                partialName = "_BlacksmithBoots";
+            }
+            else if (craft == "Weapons")
+            {
+                partialName = "_BlacksmithWeapons";
+            }
+            else
+            {
+                return this.BadRequest("Not found");
+            }
+
+
+            return this.View((object)partialName);
+        }
+    }
+}
